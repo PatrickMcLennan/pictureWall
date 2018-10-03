2ddf2d8974049d4033e47510234e719cbad39ab3
Patrick <patrick.a.mclennan@gmail.com>
2018-10-03 15:23:53 -0400

    more api work, done unsplash + nasa

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-const unsplashKey = "https://api.unsplash.com/photos/?client_id=48ccb7bdf03ecfcfa3702027cf44a1a82a54947daedc60147dbb4af9b7690980";
 const catKey = "b04b6cc1-241a-443e-821d-a71c6a83a89d";
 const catUserId = "ttfd3r"  // ??
 const googleCanadaKey = "https://newsapi.org/v2/top-headlines?country=canada&apiKey=8baaf90261984e748f990e495360e903"
@@ -19,4 +18,16 @@ async function nasa() {
     nasa.querySelector('.pic__header').innerText = json.title;
     nasa.querySelector('.pic__description').innerText = json.explanation;
 }
-nasa();
+
+async function unsplash() {
+    const unsplashKey = "https://api.unsplash.com/photos/?client_id=48ccb7bdf03ecfcfa3702027cf44a1a82a54947daedc60147dbb4af9b7690980";
+    const jsonList = await getJSON(unsplashKey);
+    const json = jsonList[Math.floor(Math.random() * jsonList.length)];
+    const unsplash = document.querySelector('.unsplash');
+    unsplash.style.backgroundImage = `url(${json.urls.regular})`;
+    // unsplash.document.querySelector('.pic__header').innerText = json.
+    console.log(json);
+}
+
+// nasa();
+// unsplash();
