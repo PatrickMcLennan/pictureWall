7fe8d9a15735504f464230955c5d14ee4b0b85f2
Patrick <patrick.a.mclennan@gmail.com>
2018-10-03 22:27:35 -0400

    api done, tweaking css

--- main.js
+++ main.js
@@ -8,33 +8,33 @@ async function getJSON(apiKey) {
 async function nasa() {
     const nasaKey = "https://api.nasa.gov/planetary/apod?api_key=hnReIst50ddj32BBbcm5dkbpPT3XpyusdPwXYf17";
     const json = await getJSON(nasaKey);
-    const nasa = document.querySelector('.nasa');
-    nasa.style.backgroundImage = `url(${json.url})`;
-    nasa.querySelector('.pic__description').innerText = json.explanation;
+    const nasaDOM = document.querySelector('.nasa');
+    nasaDOM.style.backgroundImage = `url(${json.url})`;
+    nasaDOM.querySelector('.pic__description').innerText = json.explanation;
 }
 
 async function unsplash() {
     const unsplashKey = "https://api.unsplash.com/photos/?client_id=48ccb7bdf03ecfcfa3702027cf44a1a82a54947daedc60147dbb4af9b7690980";
     const jsonList = await getJSON(unsplashKey);
     const json = jsonList[Math.floor(Math.random() * jsonList.length)];
-    const unsplash = document.querySelector('.unsplash');
-    unsplash.style.backgroundImage = `url(${json.urls.regular})`;
+    const unsplashDOM = document.querySelector('.unsplash');
+    unsplashDOM.style.backgroundImage = `url(${json.urls.regular})`;
 }
 
 async function cat() {
     const catKey = 'https://api.thecatapi.com/v1/images/search?';
     const jsonArray = await getJSON(catKey);
     const json = jsonArray[0];
-    const cat = document.querySelector('.cat');
-    cat.style.backgroundImage = `url(${json.url})`;
+    const catDOM = document.querySelector('.cat');
+    catDOM.style.backgroundImage = `url(${json.url})`;
 }
 
 async function dog() {
     const dogKey = 'https://api.thedogapi.com/v1/images/search?';
     const jsonArray = await getJSON(dogKey);
     const json = jsonArray[0];
-    const dog = document.querySelector('.dog');
-    dog.style.backgroundImage = `url(${json.url})`;
+    const dogDOM = document.querySelector('.dog');
+    dogDOM.style.backgroundImage = `url(${json.url})`;
 }
 
 async function gfycat() {
@@ -52,14 +52,14 @@ async function nyt() {
     const jsonResults = jsonObj.results;
     const json = jsonResults[Math.floor(Math.random() * jsonResults.length)];
     const nytDOM = document.querySelector('.nyt');
-    nytDOM.style.backgroundImage = `url(${json.multimedia[2].url})`;
+    nytDOM.style.backgroundImage = `url(${json.multimedia[4].url})`;
     nytDOM.querySelector('.pic__description').innerText = `${json.abstract}`;
     console.log(json);
 
 }
-nasa();
-unsplash();
-cat();
-dog();
-gfycat();
-nyt();
+// nasa();
+// unsplash();
+// cat();
+// dog();
+// gfycat();
+// nyt();
